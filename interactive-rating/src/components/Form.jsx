a19b3df2f2bfc5adf533260d9ba3b58a5ad21101
Jorge Echeverria <coko.eche@gmail.com>
2022-10-13 14:13:48 +0200

    add files

---
+++ interactive-rating/src/components/Form.jsx
@@ -0,0 +1,16 @@
+import IconStar from '../images/icon-star.svg'
+
+const Form = () => {
+  return (
+    <div>
+        <img 
+        src={IconStar} alt="icono estrella" />
+        <h1>How did we do?</h1>
+        <p>Please let us know how we did with your support request. All feedback is appreciated 
+  to help us improve our offering!</p>
+        <form action=""></form>
+    </div>
+  )
+}
+
+export default Form
